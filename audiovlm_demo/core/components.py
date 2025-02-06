0e38e49dbfaf17caaab06f09839c7459336570d8
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2025-02-05 23:58:07 -0800

    Start initializer for `AudioVLM`

--- audiovlm_demo/core/components.py
+++ audiovlm_demo/core/components.py
@@ -2,5 +2,20 @@ from audiovlm_demo import Config
 
 
 class AudioVLM:
-    def __init__(config: Config):
+    def __init__(self, *, config: Config, model_store: dict | None = None):
         self.config = config
+        model_store_keys = {"Loaded", "History", "Model", "Processor"}
+        if model_store is not None:
+            if not model_store_keys <= model_store.keys():
+                raise ValueError(
+                    "Argument `model_store` is missing the following "
+                    f"keys: {model_store_keys - model_store.keys()}"
+                )
+            self.model_store = model_store
+        else:
+            self.model_store = {
+                "Loaded": False,
+                "History": [],
+                "Model": None,
+                "Processor": None,
+            }
