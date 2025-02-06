85971864654218c89f45448918e73b6e7d690714
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2025-02-06 03:56:56 -0800

    Add Add `AudioVLM.load_model`

--- audiovlm_demo/core/components.py
+++ audiovlm_demo/core/components.py
@@ -4,6 +4,12 @@ import re
 from audiovlm_demo.core.config import Config
 
 import torch
+from transformers import (
+    AutoModelForCausalLM,
+    AutoProcessor,
+    BitsAndBytesConfig,
+    Qwen2AudioForConditionalGeneration,
+)
 
 
 class AudioVLM:
@@ -37,6 +43,94 @@ class AudioVLM:
             self.model_store["Model"] = None
             self.model_store["Processor"] = None
             self.model_store["Loaded"] = False
+
+    def load_model(self, model_selection: str):
+        # Placeholder
+        # global # model_info_pane
+        if self.model_store["Model"]:
+            self.model_cleanup()
+
+        match model_selection:
+            case "Molmo-7B-D-0924":
+                # Placeholder for Panel UI
+                # model_info_pane.object = f"<p>Loading {model_selection}...</p>"
+                self.model_store["Processor"] = AutoProcessor.from_pretrained(
+                    self.config.model_path,
+                    trust_remote_code=True,
+                    torch_dtype=torch.bfloat16,
+                    device_map="auto",
+                )
+                self.model_store["Model"] = AutoModelForCausalLM.from_pretrained(
+                    self.config.model_path,
+                    trust_remote_code=True,
+                    torch_dtype=torch.bfloat16,
+                    device_map="auto",
+                )
+                # Placeholder for Panel UI
+                # model_info_pane.object = f"<p>{model_selection} loaded.</p>"
+                self.model_store["Loaded"] = True
+            case "Molmo-7B-D-0924-4bit":
+                # Placeholder for Panel UI
+                # model_info_pane.object = f"<p>Loading {model_selection}...</p>"
+                self.model_store["Processor"] = AutoProcessor.from_pretrained(
+                    self.config.model_path,
+                    trust_remote_code=True,
+                    torch_dtype=torch.bfloat16,
+                    device_map="auto",
+                )
+                arguments = {
+                    "device_map": "auto",
+                    "torch_dtype": "auto",
+                    "trust_remote_code": True,
+                }
+                quantization_config = BitsAndBytesConfig(
+                    load_in_4bit=True,
+                    bnb_4bit_quant_type="fp4",  # or nf4
+                    bnb_4bit_use_double_quant=False,
+                )
+                arguments["quantization_config"] = quantization_config
+                self.model_store["Model"] = AutoModelForCausalLM.from_pretrained(
+                    self.config.model_path,
+                    **arguments,
+                )
+                # Placeholder for Panel UI
+                # model_info_pane.object = f"<p>{model_selection} loaded.</p>"
+                self.model_store["Loaded"] = True
+            case "Aria":
+                # Placeholder for Panel UI
+                # model_info_pane.object = f"<p>Loading {model_selection}...</p>"
+
+                model_id_or_path = self.config.aria_model_path
+                self.model_store["Processor"] = AutoProcessor.from_pretrained(
+                    model_id_or_path, trust_remote_code=True
+                )
+                self.model_store["Model"] = AutoModelForCausalLM.from_pretrained(
+                    model_id_or_path,
+                    device_map="auto",
+                    torch_dtype=torch.bfloat16,
+                    trust_remote_code=True,
+                )
+                # Placeholder for Panel UI
+                # model_info_pane.object = f"<p>{model_selection} loaded.</p>"
+                self.model_store["Loaded"] = True
+            case "Qwen2-Audio":
+                # Placeholder for Panel UI
+                # model_info_pane.object = f"<p>Loading {model_selection}...</p>"
+
+                model_id_or_path = self.config.qwen_audio_model_path
+                self.model_store["Processor"] = AutoProcessor.from_pretrained(
+                    model_id_or_path
+                )
+                self.model_store["Model"] = (
+                    Qwen2AudioForConditionalGeneration.from_pretrained(
+                        model_id_or_path, device_map="auto"
+                    )
+                )
+                # Placeholder for Panel UI
+                # model_info_pane.object = f"<p>{model_selection} loaded.</p>"
+                self.model_store["Loaded"] = True
+            case _:
+                pass
 
     @classmethod
     def parse_points(cls, points_str: str):
