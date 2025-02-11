2266f7e6e28647842a36c8b746e5988145ad4a6b
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2025-02-10 20:53:10 -0800

    Move `Config` to `components` module

--- audiovlm_demo/__init__.py
+++ audiovlm_demo/__init__.py
@@ -4,6 +4,5 @@ __all__ = [
     "Config",
 ]
 
-from audiovlm_demo.core.components import AudioVLM
-from audiovlm_demo.core.config import Config
+from audiovlm_demo.core.components import AudioVLM, Config
 from audiovlm_demo.ui.panel import AudioVLMPanel
--- audiovlm_demo/core/components.py
+++ audiovlm_demo/core/components.py
@@ -2,10 +2,14 @@ import gc
 import io
 import re
 import time
-from typing import Any
+from pathlib import Path
+from typing import Annotated, Any
 
 import librosa
+import tomlkit
 import torch
+from pydantic import AfterValidator
+from pydantic_settings import BaseSettings
 from transformers import (
     AutoModelForCausalLM,
     AutoProcessor,
@@ -14,7 +18,32 @@ from transformers import (
     Qwen2AudioForConditionalGeneration,
 )
 
-from audiovlm_demo.core.config import Config
+from audiovlm_demo.core.utils import resolve_path
+
+_ResolvedPath = Annotated[Path, AfterValidator(resolve_path)]
+
+
+class Config(BaseSettings):
+    """
+    Class to store configuration for the demo.
+
+    Includes paths to downloaded models, among other thnigs.
+    """
+
+    model_path: _ResolvedPath
+    aria_model_path: _ResolvedPath
+    qwen_audio_model_path: _ResolvedPath
+
+    # TODO: I would like to remove the quotes in the return type
+    # annotation without getting a NameError
+    @classmethod
+    def from_file(cls, path: str | Path) -> "Config":
+        path = resolve_path(path)
+        if not path.is_file():
+            raise FileNotFoundError(f"{path} does not exist.")
+
+        with open(path) as file:
+            return cls.model_validate(tomlkit.load(file).unwrap())
 
 
 class AudioVLM:
