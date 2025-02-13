77eacc53d60f4bbe688c0d57360dba8111a0a474
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2025-02-13 08:17:10 +0000

    Add placeholder values in `main.py`

--- audiovlm_demo/main.py
+++ audiovlm_demo/main.py
@@ -2,7 +2,11 @@ from audiovlm_demo import AudioVLM, AudioVLMPanel, Config
 
 
 def main():
-    config = Config.from_file("config.toml")
+    config = Config(
+        model_path="allenai/Molmo-7B-D-0924",
+        aria_model_path="rhymes-ai/Aria",
+        qwen_audio_model_path="Qwen/Qwen2-Audio-7B-Instruct",
+    )
     A = AudioVLM(config=config)
     UI = AudioVLMPanel(engine=A)  # noqa: F841
 
